c4a012933447293c7fc3c2ca3c0e2f1ed2f5e0d7
Rodrigobrands <155950369+Rodrigobrands@users.noreply.github.com>
2024-07-18 03:34:12 -0300

    Add files via upload

---
+++ iniciar_instacias_separadas.bat
@@ -0,0 +1,5 @@
+@echo off
+title Impulso ON
+py main.py
+start main.py
+pause.
---
+++ instalar_dependecias.bat
@@ -0,0 +1,6 @@
+@echo off
+title d3vil rei do sexo
+@echo instalando essas bomba ...
+pip install -r modulos.txt
+@echo instalo graças a deus pai receba
+pause
